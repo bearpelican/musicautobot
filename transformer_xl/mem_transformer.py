0b396c73475973c732400ea1125984f576044ba6
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-02-06 04:29:07 +0000

    Initial transformer code works

--- transformer_xl/mem_transformer.py
+++ transformer_xl/mem_transformer.py
@@ -735,30 +735,34 @@ class MemTransformerLM(nn.Module):
 
         return core_out, new_mems
 
-    def forward(self, data, target, *mems):
-        # nn.DataParallel does not allow size(0) tensors to be broadcasted.
-        # So, have to initialize size(0) mems inside the model forward.
-        # Moreover, have to return new_mems to allow nn.DataParallel to piece
-        # them together.
+    def forward(self, data, mems):
         if not mems: mems = self.init_mems()
+        return self._forward(data, mems=mems)
+        
+#     def forward(self, data, target, mems):
+#         # nn.DataParallel does not allow size(0) tensors to be broadcasted.
+#         # So, have to initialize size(0) mems inside the model forward.
+#         # Moreover, have to return new_mems to allow nn.DataParallel to piece
+#         # them together.
+#         if not mems: mems = self.init_mems()
 
-        tgt_len = target.size(0)
-        hidden, new_mems = self._forward(data, mems=mems)
+#         tgt_len = target.size(0)
+#         hidden, new_mems = self._forward(data, mems=mems)
 
-        pred_hid = hidden[-tgt_len:]
-        if self.sample_softmax > 0 and self.training:
-            assert self.tie_weight
-            logit = sample_logits(self.word_emb,
-                self.out_layer.bias, target, pred_hid, self.sampler)
-            loss = -F.log_softmax(logit, -1)[:, :, 0]
-        else:
-            loss = self.crit(pred_hid.view(-1, pred_hid.size(-1)), target.view(-1))
-            loss = loss.view(tgt_len, -1)
+#         pred_hid = hidden[-tgt_len:]
+#         if self.sample_softmax > 0 and self.training:
+#             assert self.tie_weight
+#             logit = sample_logits(self.word_emb,
+#                 self.out_layer.bias, target, pred_hid, self.sampler)
+#             loss = -F.log_softmax(logit, -1)[:, :, 0]
+#         else:
+#             loss = self.crit(pred_hid.view(-1, pred_hid.size(-1)), target.view(-1))
+#             loss = loss.view(tgt_len, -1)
 
-        if new_mems is None:
-            return [loss]
-        else:
-            return [loss] + new_mems
+#         if new_mems is None:
+#             return [loss]
+#         else:
+#             return [loss] + new_mems
 
 if __name__ == '__main__':
     import argparse
