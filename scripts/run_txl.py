5c804297c9754753454e8db0e9950df0dead59b2
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-05-30 18:26:20 +0000

    DELETING all old notebooks

--- scripts/run_txl.py
+++ scripts/run_txl.py
@@ -1,23 +1,36 @@
-from fastai.text import *
-from fastai.callbacks.tracker import *
-from fastai.callbacks.rnn import RNNTrainer
+
+import music21
+import torch
+
 from fastai.distributed import *
-from fastai_data import *
-from transformer_xl import default_txl
+from fastai.text.models.transformer import *
+
+import numpy as np
+
+import sys
+sys.path.insert(0, '..')
+from src.fastai_data import *
+from src.lmnp_transformer import *
+from src.serve import *
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', type=str, default='data/midi/midi_transcribe_v3_shortdur/')
-parser.add_argument('--cache', type=str, default='tmp_clc')
+parser.add_argument('--path', type=str, default='../data/midi/v15/midi_encode/')
+parser.add_argument('--cache', type=str, default='tmp/dmp')
 parser.add_argument('--save', type=str, default='first_run')
 parser.add_argument('--load', type=str, default=None)
 parser.add_argument("--local_rank", type=int)
-parser.add_argument("--batch_size", type=int, default=8)
-parser.add_argument("--bptt", type=int, default=500)
+parser.add_argument("--batch_size", type=int, default=12)
+parser.add_argument("--mem_len", type=int, default=512)
+parser.add_argument("--bptt", type=int, default=512)
 parser.add_argument('--half', action='store_true', help='Use half precision')
 parser.add_argument('--wd', type=float, default=1e-3, help='weight decay for adam')
 parser.add_argument('--epochs', type=int, default=5, help='num epochs')
 parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+parser.add_argument('--div_factor', type=int, default=10, help='learning rate div factor')
+parser.add_argument('--save_every', action='store_true', help='Save every epoch')
+parser.add_argument('--config', type=str, help='serve.py config name')
+
 args = parser.parse_args()
 
 if args.local_rank != 0:
@@ -27,47 +40,35 @@ if args.local_rank != 0:
 torch.cuda.set_device(args.local_rank)
 torch.distributed.init_process_group(backend='nccl', init_method='env://')
 
-bs=args.batch_size
-bptt=args.bptt
+
 path = Path(args.path)
-data = TextLMDataBunch.load(path, cache_name=args.cache, bs=bs, bptt=bptt)
-data.valid_ds.x.processor[0] = TokenizeProcessor(tokenizer=MusicTokenizer())
 
-vocab = data.train_ds.vocab
-vocab_size = len(vocab.itos)
+from src import serve
+config = getattr(serve, args.config)(vocab)
 
-model = default_txl.get_default_model(vocab_size).cuda()
-model.reset = lambda: None
+config['bptt'] = args.bptt
+config['bs'] = args.batch_size
+data = load_music_data(path=path, cache_name=args.cache, vocab=vocab, y_offset=1, **config)
 
-class TXLTrainer(LearnerCallback):
-    def __init__(self, learn:Learner):
-        super().__init__(learn)
-        self.mems = None
+full_clip = None if args.half else 0.5
 
-    def on_batch_begin(self, last_input, last_target, train, **kwargs):
-        # model expects bptt, batch_size
-        return (last_input, self.mems), last_target
-        
-    def on_loss_begin(self, last_output:Tensor, train, **kwargs:Any) -> Tensor:
-        out, self.mems = last_output
-        return out
+learn = music_model_learner(data, config, clip=full_clip)
 
-full_clip = None if args.half else 0.3
-learn = LanguageLearner(data, model, bptt, clip=full_clip)
 if args.load:
     load_path = Path(args.path)/args.load
     state = torch.load(load_path, map_location='cpu')
-    get_model(learn.model).load_state_dict(state['model'], strict=True)
+    get_model(learn.model).load_state_dict(state['model'], strict=False)
     learn.model.cuda()
 if args.save:
     save_path = Path(args.path)/learn.model_dir/args.save
     save_path.parent.mkdir(parents=True, exist_ok=True)
-learn.callbacks = [c for c in learn.callbacks if not isinstance(c, RNNTrainer)]
-learn.callbacks.append(TXLTrainer(learn))
-if args.half: learn = learn.to_fp16(clip=.4)
-learn = learn.distributed(args.local_rank, drop_last=True, shuffle=False)
+if args.half: learn = learn.to_fp16(clip=0.5, dynamic=True)
+learn = learn.to_distributed(args.local_rank, cache_dir=args.cache+'/dist_logs')
 if args.local_rank == 0: learn.callbacks.append(SaveModelCallback(learn, name=f'{args.save}_best'))
+if args.local_rank == 0 and args.save_every: learn.callbacks.append(SaveModelCallback(learn, name=f'{args.save}_epoch', every='epoch'))
+# learn.callbacks.append(EarlyStoppingCallback(learn))
 
-learn.fit_one_cycle(args.epochs, args.lr, div_factor=30, moms=(0.7,0.5))
+learn.fit_one_cycle(2, args.lr/2, div_factor=50, pct_start=0.9)
+learn.fit_one_cycle(args.epochs, args.lr, div_factor=args.div_factor, pct_start=0.1, final_div=50, wd=1e-4)
 
 if args.local_rank == 0: learn.save(f'{args.save}')
