06a684bf7eb023a3a3810c14e2a9a7490dea3845
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-05-31 11:35:52 +0000

    Lamb eps needs to be higher for mixed precision

--- scripts/run_txl.py
+++ scripts/run_txl.py
@@ -68,7 +68,7 @@ if args.load:
 if args.save:
     save_path = Path(args.path)/learn.model_dir/args.save
     save_path.parent.mkdir(parents=True, exist_ok=True)
-if args.half: learn = learn.to_fp16(clip=0.5, dynamic=True, loss_scale=128, max_scale=256)
+if args.half: learn = learn.to_fp16(clip=0.5, dynamic=True)
 learn = learn.to_distributed(args.local_rank, cache_dir=args.cache+'/dist_logs')
 if args.local_rank == 0: learn.callbacks.append(SaveModelCallback(learn, name=f'{args.save}_best'))
 if args.local_rank == 0 and args.save_every: learn.callbacks.append(SaveModelCallback(learn, name=f'{args.save}_epoch', every='epoch'))
--- src/lamb.py
+++ src/lamb.py
@@ -28,7 +28,7 @@ class Lamb(Optimizer):
         https://arxiv.org/abs/1904.00962
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
                  weight_decay=0, adam=False):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
