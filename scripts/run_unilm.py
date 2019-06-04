49f9c7bb248e526df374688ac9919a804163bcab
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-06-04 00:10:58 +0000

    Trying out data parallel

--- scripts/run_unilm.py
+++ scripts/run_unilm.py
@@ -33,6 +33,7 @@ parser.add_argument('--save_every', action='store_true', help='Save every epoch'
 parser.add_argument('--config', type=str, default='unilm_config', help='serve.py config name')
 parser.add_argument('--no_transpose', action='store_true', help='No transpose data augmentation')
 parser.add_argument("--ns_max_cls", type=int, default=4)
+parser.add_argument('--data_parallel', action='store_true', help='DataParallel instead of DDP')
 
 args = parser.parse_args()
 args.path = Path(args.path)
@@ -71,7 +72,8 @@ s2s_data = MusicDataBunch.load(args.path/'s2s_encode', cache_name=args.cache,
 
 nw_dl_tfms = [nw_tfm]
 nw_data = load_music_data(args.path/'piano_duet', cache_name=args.cache, vocab=vocab, dl_tfms=nw_dl_tfms, y_offset=1, **config)
-datasets = [ns_data, s2s_data, nw_data]
+#datasets = [ns_data, s2s_data, nw_data]
+datasets = [s2s_data, nw_data, ns_data]
 
 full_clip = None if args.half else 0.5
 
@@ -102,6 +104,7 @@ if args.save:
     save_path.parent.mkdir(parents=True, exist_ok=True)
 if args.half: learn = learn.to_fp16(clip=0.5, dynamic=True, max_scale=2**18)
 if is_distributed: learn = learn.to_distributed(args.local_rank, cache_dir=args.cache+'/dist_logs')
+if args.data_parallel: learn = learn.to_parallel()
 # if args.local_rank == 0: learn.callbacks.append(SaveModelCallback(learn, name=f'{args.save}_best'))
 
 if not args.lamb: learn.fit_one_cycle(2, args.lr/2, div_factor=50, pct_start=0.9) # no need for warmup with lamb
